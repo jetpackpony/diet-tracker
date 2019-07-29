568d644af4a965e91ff881afd20a43c5657381b7
jetpackpony <jetpackpony@gmail.com>
2019-07-29 16:06:27 +0300

    Add some initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet } from 'grommet';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px'
+    }
+  }
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme} >
+      <div className="food-journal">
+        <div className="day-log">
+          <div className="day-header">
+            <div className="day-date">28 Jul 2019</div>
+            <div className="day-calories">3584 ccal</div>
+            <div className="day-protein">P: 44 g</div>
+            <div className="day-fat">F: 134 g</div>
+            <div className="day-net-carbs">NC: 23 g</div>
+          </div>
+          <div className="day-entry">
+            <div className="entry-title">Cinnamon Rolls</div>
+            <div className="entry-weight">230 g</div>
+            <div className="entry-calories">670 ccal</div>
+            <div className="entry-protein">53 g</div>
+            <div className="entry-fat">23 g</div>
+            <div className="entry-net-carbs">43 g</div>
+          </div>
+        </div>
+      </div>
+    </Grommet>
   );
 }
 
