e6f30be111e634b89023d57b5bf2369833eb48f1
jetpackpony <jetpackpony@gmail.com>
2019-07-31 16:56:15 +0300

    Add AddForm form layout

--- src/components/AddForm/index.js
+++ src/components/AddForm/index.js
@@ -1,37 +1,77 @@
 import React from 'react';
+import styles from './AddForm.module.css';
 
 const AddForm = () => (
   <form>
     <h1>Add Food</h1>
-    <label htmlFor="title">
-      <span>Food: </span>
-    </label>
-    <input type="text" id="title" name="title" />
+    <div className={styles.formContainer}>
+      <div className={styles.fieldContainer}>
+        <label htmlFor="title">
+          <span>Food: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="text" id="title" name="title" placeholder="Enter food" />
+        </div>
+      </div>
 
-    <label htmlFor="weight">
-      <span>Weight: </span>
-    </label>
-    <input type="number" id="weight" name="weight" />
+      <div className={styles.fieldContainer}>
+        <label htmlFor="weight">
+          <span>Weight: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="number" id="weight" name="weight" />
+        </div>
+      </div>
 
-    <label htmlFor="calories">
-      <span>Ccal: </span>
-    </label>
-    <input type="number" id="calories" name="calories" />
+      <div className={styles.fieldContainer}>
+        <label htmlFor="calories">
+          <span>Ccal: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="number" id="calories" name="calories" />
+        </div>
+      </div>
 
-    <label htmlFor="protein">
-      <span>Protein: </span>
-    </label>
-    <input type="number" id="protein" name="protein" />
+      <div className={styles.fieldContainer}>
+        <label htmlFor="protein">
+          <span>Protein: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="number" id="protein" name="protein" />
+        </div>
+      </div>
 
-    <label htmlFor="fat">
-      <span>Fat: </span>
-    </label>
-    <input type="number" id="fat" name="fat" />
+      <div className={styles.fieldContainer}>
+        <label htmlFor="fat">
+          <span>Fat: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="number" id="fat" name="fat" />
+        </div>
+      </div>
 
-    <label htmlFor="carbs">
-      <span>Net Carbs: </span>
-    </label>
-    <input type="number" id="carbs" name="carbs" />
+      <div className={styles.fieldContainer}>
+        <label htmlFor="carbs">
+          <span>Net Carbs: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="number" id="carbs" name="carbs" />
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <label htmlFor="date">
+          <span>Date: </span>
+        </label>
+        <div className={styles.inputContainer}>
+          <input type="date" id="date" name="date" />
+        </div>
+      </div>
+
+      <div className={styles.fieldContainer}>
+        <input type="submit" id="submit" name="submit" value="Submit" />
+      </div>
+    </div>
   </form>
 
 );
