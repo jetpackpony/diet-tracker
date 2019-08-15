2a3dae6d6f65c062c5db3ebe43789025e6b113e8
jetpackpony <jetpackpony@gmail.com>
2019-08-15 16:31:25 +0300

    Remove traces of redux

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux';
-import initStore from './store';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient, { gql } from 'apollo-boost';
+import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
 });
 
-const { store, persistor } = initStore();
-
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>,
+    <App />
   </ApolloProvider>,
   document.getElementById('root')
 );
