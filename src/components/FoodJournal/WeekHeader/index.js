6ffe67f15235b79a2c4d0bdcd49fb8c06a3356d1
jetpackpony <jetpackpony@gmail.com>
2020-01-27 11:18:06 +0300

    Fix week headers

--- src/components/FoodJournal/WeekHeader/index.js
+++ src/components/FoodJournal/WeekHeader/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import styles from './WeekHeader.module.css';
 import moment from 'moment';
 
-const getDateString = (isoString) => moment(isoString).format("D MMMM");
+const weekHeaderFormat = "D MMMM";
 
 const WeekHeader = ({
   weekStart,
   weekEnd,
   calDeficit,
 }) => {
+  const newStart = moment(weekStart).add(6, "hours");
+  const newEnd = moment(weekEnd).subtract(6, "hours");
   return (
     <header className={styles.weekHeader}>
-      <h2>{getDateString(weekStart)} - {getDateString(weekEnd)}</h2>
+      <h2>
+        {newStart.format(weekHeaderFormat)} - {newEnd.format(weekHeaderFormat)}
+      </h2>
       <div>{calDeficit} left</div>
     </header>
   )
