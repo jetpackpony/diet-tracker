ffe8f8996ed334a1118c7d3fa823fa0f8c17b34c
jetpackpony <jetpackpony@gmail.com>
2022-06-09 16:40:36 +0300

    Add new styles to buttons

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -6,12 +6,106 @@ import { action } from '@storybook/addon-actions';
 import Button from './index';
 import { Trash } from 'grommet-icons';
 
-storiesOf('Button', module)
+const Container = ({ children }) => {
+  return (
+    <div style={{ padding: "20px", background: "var(--content-color)", height: "100vh", width: "100vw" }}>
+      {children}
+    </div>
+  );
+};
+
+storiesOf('Button/Primary', module)
   .add('with icon', () => (
-    <Button
-      onClick={action("clicked")}
-    >
-      <Trash size="small" color="red" />
-      <span> Delete</span>
-    </Button>
+    <Container>
+      <Button
+        onClick={action("clicked")}
+        icon={Trash}
+        text="Delete"
+      >
+      </Button>
+    </Container>
   ))
+  .add('text only', () => (
+    <Container>
+      <Button
+        onClick={action("clicked")}
+        text="Delete"
+      >
+      </Button>
+    </Container>
+  ))
+  .add('icon only', () => (
+    <Container>
+      <Button
+        onClick={action("clicked")}
+        icon={Trash}
+      >
+      </Button>
+    </Container>
+  ));
+
+storiesOf('Button/Outlined', module)
+  .add('with icon', () => (
+    <Container>
+      <Button
+        type="outlined"
+        onClick={action("clicked")}
+        icon={Trash}
+        text="Delete"
+      >
+      </Button>
+    </Container>
+  ))
+  .add('text only', () => (
+    <Container>
+      <Button
+        type="outlined"
+        onClick={action("clicked")}
+        text="Delete"
+      >
+      </Button>
+    </Container>
+  ))
+  .add('icon only', () => (
+    <Container>
+      <Button
+        type="outlined"
+        onClick={action("clicked")}
+        icon={Trash}
+      >
+      </Button>
+    </Container>
+  ));
+
+storiesOf('Button/PlainText', module)
+  .add('with icon', () => (
+    <Container>
+      <Button
+        type="plainText"
+        onClick={action("clicked")}
+        icon={Trash}
+        text="Delete"
+      >
+      </Button>
+    </Container>
+  ))
+  .add('text only', () => (
+    <Container>
+      <Button
+        type="plainText"
+        onClick={action("clicked")}
+        text="Delete"
+      >
+      </Button>
+    </Container>
+  ))
+  .add('icon only', () => (
+    <Container>
+      <Button
+        type="plainText"
+        onClick={action("clicked")}
+        icon={Trash}
+      >
+      </Button>
+    </Container>
+  ));
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from './Button.module.css';
 
-const Button = ({ className, children, ...props }) => {
+const Button = ({
+  className,
+  icon = null,
+  text = "",
+  type = "primary",
+  onClick,
+  buttonProps = { type: "submit", name: "submit" }
+}) => {
+  const [inkPos, setInkPos] = useState({ top: 0, left: 0, size: 0 });
+  const [inkActive, setInkActive] = useState(false);
+  const buttonOnClick = (e) => {
+    const buttonRect = e.currentTarget.getBoundingClientRect();
+    const size = Math.max(buttonRect.width, buttonRect.height);
+    const top = e.clientY - buttonRect.top - size / 2;
+    const left = e.clientX - buttonRect.left - size / 2;
+    setInkPos({ top, left, size });
+    setInkActive(true);
+    setTimeout(() => setInkActive(false), 400);
+    onClick(e);
+  };
+
+  // We need this to be able to use prop as component
+  const Icon = icon;
+
+  const primaryColorText = getComputedStyle(document.documentElement).getPropertyValue('--primary-color-text');
+  const primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--primary-color');
+  let iconColor = "";
+  const buttonStyles = [className, styles.button];
+  switch (type) {
+    case "outlined":
+      buttonStyles.push(styles.outlined);
+      iconColor = primaryColor;
+      break;
+    case "plainText":
+      buttonStyles.push(styles.plainText);
+      iconColor = primaryColor;
+      break;
+    case "primary":
+    default:
+      buttonStyles.push(styles.primary);
+      iconColor = primaryColorText;
+  }
+
   return (
-    <button className={[className, styles.button].join(" ")} {...props}>
-      {children}
+    <button
+      className={buttonStyles.join(" ")}
+      onClick={buttonOnClick}
+      {...buttonProps}
+    >
+
+      <span style={{ marginRight: (icon && text) ? "0.5rem" : "0" }}>
+        {icon && <Icon size="small" color={iconColor} />}
+      </span>
+      <span>{text}</span>
+      <span
+        className={[styles.ink, (inkActive) ? styles.inkActive : ""].join(" ")}
+        style={{ top: inkPos.top, left: inkPos.left, width: inkPos.size, height: inkPos.size }}
+      ></span>
     </button>
   )
 };
