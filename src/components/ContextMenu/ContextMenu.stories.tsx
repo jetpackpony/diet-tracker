1e0d70e7c0fc1c3506aac0393bff6dbfa1419bf9
jetpackpony <jetpackpony@gmail.com>
2022-07-20 15:01:19 +0300

    Rename storybook files to ts

--- src/components/ContextMenu/ContextMenu.stories.tsx
+++ src/components/ContextMenu/ContextMenu.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import ContextMenu from './index.js';
+import ContextMenu from './index';
 
 storiesOf('ContextMenu', module)
   .add('default', () => (
