cbd6d02766ec9fe9e82c207128946d06f2404a0f
jetpackpony <jetpackpony@gmail.com>
2022-07-27 16:32:45 +0300

    Add types to cache operations

--- src/utils/cacheOperations.ts
+++ src/utils/cacheOperations.ts
@@ -1,8 +1,8 @@
 import { ApolloCache } from "@apollo/client";
 import moment from "moment";
-import { WeeklyRecordsFeedDocument } from "../generated/graphql";
+import { Record, WeeklyRecordsFeedDocument, WeekRecords } from "../generated/graphql";
 
-const deleteRecord = (weeks, recId) => {
+const deleteRecord = (weeks: WeekRecords[], recId: string) => {
   const res = weeks.map((week) => {
     return {
       ...week,
@@ -17,7 +17,7 @@ const deleteRecord = (weeks, recId) => {
   return res;
 };
 
-const updateTotals = (weeks) => {
+const updateTotals = (weeks: WeekRecords[]) => {
   return weeks.map((week) => {
     const days = week.days.map((day) => {
       const totals = day.records.reduce((acc, rec) => {
@@ -66,80 +66,83 @@ const updateTotals = (weeks) => {
   });
 };
 
-export const updateCachedTotals = (cache, data) => {
-  const { weeklyRecordsFeed } = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
-
-  const newData = {
-    weeklyRecordsFeed: {
-      ...weeklyRecordsFeed,
-      weeks: updateTotals(weeklyRecordsFeed.weeks)
-    }
-  };
-  cache.writeQuery({
-    query: WeeklyRecordsFeedDocument,
-    variables: { cursor: "" },
-    data: newData
-  });
+export const updateCachedTotals = (cache: ApolloCache<any>) => {
+  const weeklyRecordsFeedQuery = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
+  if (weeklyRecordsFeedQuery) {
+    const newData = {
+      weeklyRecordsFeed: {
+        ...weeklyRecordsFeedQuery.weeklyRecordsFeed,
+        weeks: updateTotals(weeklyRecordsFeedQuery.weeklyRecordsFeed.weeks)
+      }
+    };
+    cache.writeQuery({
+      query: WeeklyRecordsFeedDocument,
+      variables: { cursor: "" },
+      data: newData
+    });
+  }
 };
 
 export const removeRecordFromCache = (cache: ApolloCache<any>, recId: string) => {
-  const { weeklyRecordsFeed } = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
-
-  const newData = {
-    weeklyRecordsFeed: {
-      ...weeklyRecordsFeed,
-      weeks: updateTotals(deleteRecord(weeklyRecordsFeed.weeks, recId))
-    }
-  };
-  cache.writeQuery({
-    query: WeeklyRecordsFeedDocument,
-    variables: { cursor: "" },
-    data: newData
-  });
+  const weeklyRecordsFeedQuery = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
+  if (weeklyRecordsFeedQuery) {
+    const newData = {
+      weeklyRecordsFeed: {
+        ...weeklyRecordsFeedQuery.weeklyRecordsFeed,
+        weeks: updateTotals(deleteRecord(weeklyRecordsFeedQuery.weeklyRecordsFeed.weeks, recId))
+      }
+    };
+    cache.writeQuery({
+      query: WeeklyRecordsFeedDocument,
+      variables: { cursor: "" },
+      data: newData
+    });
+  }
 };
 
-export const insertRecordIntoCache = (cache, newRecord) => {
-  const { weeklyRecordsFeed } = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
-
-  const newWeeks = weeklyRecordsFeed.weeks.map((week) => {
-    if (moment(newRecord.eatenAt).isBetween(week.weekStart, week.weekEnd)) {
-      return {
-        ...week,
-        days: week.days.map((day) => {
-          if (moment(newRecord.eatenAt).isBetween(day.dayStart, day.dayEnd)) {
-            return {
-              ...day,
-              records: ([
-                ...day.records,
-                newRecord
-              ]).sort((a, b) => {
-                const diff = moment(b.eatenAt) - moment(a.eatenAt);
-                if (diff === 0) {
-                  const diffCreate = moment(b.createdAt) - moment(a.createdAt);
-                  return diffCreate;
-                } else {
-                  return diff;
-                }
-              })
-            };
-          } else {
-            return day;
-          }
-        })
-      };
-    } else {
-      return week;
-    }
-  });
-  const newData = {
-    weeklyRecordsFeed: {
-      ...weeklyRecordsFeed,
-      weeks: updateTotals(newWeeks)
-    }
-  };
-  cache.writeQuery({
-    query: WeeklyRecordsFeedDocument,
-    variables: { cursor: "" },
-    data: newData
-  });
+export const insertRecordIntoCache = (cache: ApolloCache<any>, newRecord: Record) => {
+  const weeklyRecordsFeedQuery = cache.readQuery({ query: WeeklyRecordsFeedDocument, variables: { cursor: "" } });
+  if (weeklyRecordsFeedQuery) {
+    const newWeeks = weeklyRecordsFeedQuery.weeklyRecordsFeed.weeks.map((week) => {
+      if (moment(newRecord.eatenAt).isBetween(week.weekStart, week.weekEnd)) {
+        return {
+          ...week,
+          days: week.days.map((day) => {
+            if (moment(newRecord.eatenAt).isBetween(day.dayStart, day.dayEnd)) {
+              return {
+                ...day,
+                records: ([
+                  ...day.records,
+                  newRecord,
+                ]).sort((a, b) => {
+                  const diff = moment(b.eatenAt).diff(moment(a.eatenAt));
+                  if (diff === 0) {
+                    const diffCreate = moment(b.createdAt).diff(moment(a.createdAt));
+                    return diffCreate;
+                  } else {
+                    return diff;
+                  }
+                })
+              };
+            } else {
+              return day;
+            }
+          })
+        };
+      } else {
+        return week;
+      }
+    });
+    const newData = {
+      weeklyRecordsFeed: {
+        ...weeklyRecordsFeedQuery.weeklyRecordsFeed,
+        weeks: updateTotals(newWeeks)
+      }
+    };
+    cache.writeQuery({
+      query: WeeklyRecordsFeedDocument,
+      variables: { cursor: "" },
+      data: newData
+    });
+  }
 };
