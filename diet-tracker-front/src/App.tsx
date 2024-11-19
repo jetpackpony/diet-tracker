3816a7862288b1f514694de3fb4b1afadd424e0d
jetpackpony <jetpackpony@gmail.com>
2024-11-19 12:48:23 +0300

    Add some test code for adding/deleting items

--- diet-tracker-front/src/App.tsx
+++ diet-tracker-front/src/App.tsx
@@ -1,5 +1,62 @@
+import PocketBase from "pocketbase";
 import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+
+let pb: PocketBase;
+function getPB() {
+  if (!pb) {
+    pb = new PocketBase("http://127.0.0.1:8090");
+  }
+  return pb;
+}
+
+function useFoodItems() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getPB()
+      .collection("foodItems")
+      .getFullList()
+      .then((items) => setItems(items));
+  }, []);
+
+  useEffect(() => {
+    getPB()
+      .collection("foodItems")
+      .subscribe("*", (e) => {
+        setItems([...items, e.record]);
+      });
+    return () => getPB().collection("foodItems").unsubscribe("*");
+  }, [items]);
+
+  return items;
+}
+
+async function createFoodItem(title: string) {
+  return await getPB().collection("foodItems").create({ title });
+}
 
 export default function App() {
-  return <div>Red text</div>;
+  const items = useFoodItems();
+  return (
+    <div>
+      <div>
+        {items.map(({ title }) => (
+          <div key={title}>{title}</div>
+        ))}
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const title = new FormData(e.target as HTMLFormElement).get("title");
+          if (title) {
+            createFoodItem(title.toString());
+          }
+        }}
+      >
+        <input name="title" type="text" />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 }
