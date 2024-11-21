9688dbb6772a3942eef2aa3e96cf2b67b5f09653
jetpackpony <jetpackpony@gmail.com>
2024-11-21 13:15:32 +0300

    Add basic PWA functionality

--- diet-tracker-front/vite.config.ts
+++ diet-tracker-front/vite.config.ts
@@ -1,7 +1,54 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: [
+        "icons/favicon.ico",
+        "icons/apple-touch-icon.png",
+        "icons/mask-icon.svg",
+      ],
+      manifest: {
+        name: "Diet Tracker",
+        short_name: "Diet Tracker",
+        description: "Track your calories and macros",
+        theme_color: "#4b2ac1",
+        icons: [
+          {
+            src: "icons/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icons/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      workbox: {
+        // defining cached files formats
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
 });
